88ddeb9e9fe9938ff062c5bbadb0d23fe0087491
christos <cmagganas@gmail.com>
2024-01-25 10:08:04 -0800

    adding readme info

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# summaRAGizer
-A Python-based web app leveraging FastAPI, LangChain, and OpenAI's GPT-4 for concise and coherent LLM-driven summarization of uploaded text documents.
+# SummaRAGizer
+
+SummaRAGizer is a Python web application that leverages AI to summarize text documents. Utilizing the power of LangChain and OpenAI's GPT-4, it provides users with a concise and coherent summary of uploaded text documents.
+
+## Objective
+
+The goal of this application is to create a user-friendly interface where users can upload text documents and receive a summarized version. The application is designed to handle documents of various lengths and topics, ensuring a high-quality summary is generated for each submission.
+
+## Features
+
+- Python-based web application with an upload endpoint.
+- Integration of LangChain and OpenAI's GPT-4 for AI-powered text summarization.
+- Accepts `.txt` file formats for summarization.
+- Error handling for file uploads, API limits, and other exceptions.
+
+## Installation
+
+You will need an [OpenAI API key](https://platform.openai.com/account/api-keys).
+To set up and run SummaRAGizer on your local machine, follow these steps:
+
+1. Clone the Repository:
+```bash
+git clone https://github.com/cmagganas/summaRAGizer.git
+cd summaRAGizer
+pip install -r requirements.txt
+export OPENAI_API_KEY="sk-123..."
+python app.py
+```
+
+2. Usage
+
+    Once the application is running, navigate to http://localhost:8000/ in your web browser. You will be greeted with a simple interface for uploading your text documents.
+
+3. Uploading Documents
+
+    Click on the 'Upload' button.
+    Select a .txt file from your device.
+    Click 'Submit' to upload the file.
+    The summarized content will be displayed on the screen.
+
+## Demo Video
+
+[Demo: Summarizing Uploaded Docs](https://www.loom.com/share/7f46586938764b72b17ff35b1b06b97c)
--- summarizer.py
+++ summarizer.py
@@ -6,14 +6,13 @@ load_dotenv()
 from langchain.chains import MapReduceDocumentsChain, ReduceDocumentsChain
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.chains.llm import LLMChain
-from langchain.chains.summarize import load_summarize_chain
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_openai import ChatOpenAI
 
 
-def summarizer(file_path, method="stuff"):
+def summarizer(file_path):
 
     docs = TextLoader(file_path).load()
 
@@ -26,22 +25,16 @@ def summarizer(file_path, method="stuff"):
     prompt = PromptTemplate.from_template(prompt_template)
 
     # Initialize the model and output parser
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")  # Update to GPT 4 when finished with app development
     llm_chain = LLMChain(llm=llm, prompt=prompt)
 
-    # Define StuffDocumentsChain
-    if method == "stuff":
-        # check if the document is too long
-        if len(docs[0].page_content) > 4097:
-            print("Document is too long. Skipping Stuff Method.")
-            return None
-
+    # Use StuffDocumentsChain if document length is within limits
+    if len(docs[0].page_content) <= 4097:
         stuff_chain = StuffDocumentsChain(llm_chain=llm_chain, document_variable_name="text")
         return stuff_chain.invoke(docs)["output_text"]
-    
-    # Map-Reduce
-    if method == "map-reduce":
 
+    # Map-Reduce method for longer documents
+    else:
         # Map
         map_template = """The following is a set of documents
         {docs}
@@ -67,23 +60,16 @@ def summarizer(file_path, method="stuff"):
 
         # Combines and iteratively reduces the mapped documents
         reduce_documents_chain = ReduceDocumentsChain(
-            # This is final chain that is called.
             combine_documents_chain=combine_documents_chain,
-            # If documents exceed context for `StuffDocumentsChain`
             collapse_documents_chain=combine_documents_chain,
-            # The maximum number of tokens to group documents into.
             token_max=4000,
         )
 
         # Combining documents by mapping a chain over them, then combining results
         map_reduce_chain = MapReduceDocumentsChain(
-            # Map chain
             llm_chain=map_chain,
-            # Reduce chain
             reduce_documents_chain=reduce_documents_chain,
-            # The variable name in the llm_chain to put the documents in
             document_variable_name="docs",
-            # Return the results of the map steps in the output
             return_intermediate_steps=False,
         )
 
@@ -93,21 +79,11 @@ def summarizer(file_path, method="stuff"):
         split_docs = text_splitter.split_documents(docs)
 
         return map_reduce_chain.invoke(split_docs)["output_text"]
-    
-    # Refine
-    if method == "refine":
-        text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-            chunk_size=1000, chunk_overlap=0
-        )
-        split_docs = text_splitter.split_documents(docs)
-        refine_chain = load_summarize_chain(llm, chain_type="refine")
-        return refine_chain.invoke(split_docs)["output_text"]
+
 
 # Usage example
 if __name__ == "__main__":
-    file_path = "sample_texts/alice_in_wonderland.txt"
-    method = "refine"
-    print(f"Method: {method}")
-    output = summarizer(file_path, method=method)
+    file_path = "sample_texts/text.txt"
+    output = summarizer(file_path)
     print(output)
     print("\n")
